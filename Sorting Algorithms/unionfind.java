077226d195e0422926561bc2031b60e82f7c03d9
nido <nidarshans@gmail.com>
2020-06-16 23:59:03 +0000

    abstract class/template for unionfind

---
+++ Sorting Algorithms/unionfind.java
@@ -0,0 +1,20 @@
+import java.io.File;
+import java.lang.Math;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class unionfind {
+	public static void main(String[] args) {
+		
+	}
+}
+
+abstract class uf {
+	public abstract void union(int p, int q);
+	public abstract boolean connected(int p , int q);
+}
+
+class quickfind {
+	
+}
