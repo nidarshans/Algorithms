b43b7ae0e99ea8e0cc8d9210d79fb358af9e35e9
nido <nidarshans@gmail.com>
2020-06-23 19:24:19 +0000

    added generics

--- stack.java
+++ stack.java
@@ -6,38 +6,33 @@ import java.util.Scanner;
 
 public class stack {
 	public static void main(String[] args) {
-		Stack R = new Stack();
-		R.push(10);
-		R.push(30);
-		R.push(50);
-		R.pop();
-		R.print();	
+		Stack<String> s = new Stack<String>();
+		s.push("Hello");
+		s.push("World");
+		System.out.println(s.pop().length());
 	}
 
 }
 
-class Stack {
-	private int[] v = new int[10];
+class Stack<T> {
+	private T[] v = (T[]) new Object[10];
 	private int N = 0;
 	public Stack() {}
 	public void resize(int L) {
-		int[] copy = new int[L];
+		T[] copy = (T[]) new Object[L];
 		for(int x = 0; x < L && x < v.length; x++) {
 			copy[x] = v[x];
 		}
 		v = copy;
 	}
-	public void push(int Q) {
+	public void push(T Q) {
 		if(N == v.length) resize(v.length * 2);
 		else v[N++] = Q;
 	}
-	public int pop() {
-		int item = v[--N];
-		v[N] = -1;
+	public T pop() {
+		T item = v[--N];
+		v[N] = null;
 		if(N > 0 && N <= v.length / 4) resize(v.length / 2);
 		return item;
-	}
-	public void print() {
-		for(int x : v) { System.out.print(x + ","); }
 	}
 }
