f926dc1ad3153412378b9969401e28e5c22418a1
nido <nidarshans@gmail.com>
2020-07-16 22:44:21 +0000

    changed int to double

--- Algorithms 1 (Java)/quicksort.java
+++ Algorithms 1 (Java)/quicksort.java
@@ -5,14 +5,12 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class quicksort {
-	public static void main(String[] args) {
-		int[] a = {1,2,56,4,6,7,7,7,77,846457,7,945,46,3465,86436,47,3,66,4,6,6,66,66};
-		quickSort(a, 0, a.length - 1);
-		for(int x : a) System.out.print(x + " ");
-	}
+	public quicksort() {}
+	public static void main(String[] args) {}
 
-	public static int partition(int[] arg, int l,  int h) {
-		int i = l, lt = l, gt = h, v = arg[l], swp = 0;
+	public static double partition(double[] arg, double l,  double h) {
+		int i = (int)l, lt = (int)l, gt = (int)h;
+		double v = arg[(int)l], swp = 0;
 		while(i <= gt) {
 			if(arg[i] < v) {
 				swp = arg[i];
@@ -32,9 +30,9 @@ public class quicksort {
 		return gt;
 	}
 
-	public static void quickSort(int[] arg, int l, int h) {
+	public static void quickSort(double[] arg, double l, double h) {
 		if(h <= l) return;
-		int j = partition(arg, l, h);
+		double j = partition(arg, l, h);
 		quickSort(arg, l, j - 1);
 		quickSort(arg, j + 1, h);
 	}
