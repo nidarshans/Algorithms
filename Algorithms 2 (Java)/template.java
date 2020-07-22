197206e947a44eea9e82a5570b7fb637040c9cf3
nido <nidarshans@gmail.com>
2020-07-22 21:05:51 +0000

    Algorithms 2

---
+++ Algorithms 2 (Java)/template.java
@@ -0,0 +1,8 @@
+import java.lang.Math;
+
+public class /*classname*/ {
+	public static void main(String[] args) {
+	
+	}
+
+}
---
+++ Algorithms 2 (Java)/undirectedgraph.java
@@ -0,0 +1,12 @@
+import java.lang.Math;
+
+public class undirectedgraph {
+	public static void main(String[] args) {
+	
+	}
+}
+
+class Graph {
+	final int S;
+	public Graph() {}
+}
